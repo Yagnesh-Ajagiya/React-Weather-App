bf956174ddee2798be3baa076dcba86ff681745b
Yagnesh06 <yagneshajagiya29@gmail.com>
2024-09-01 23:29:00 +0530

    Initial Commit

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import './weather.css';
+import axios from 'axios';
+
+export default function Weather() {
+    const [city, setCity] = useState('');
+    const [weatherInfo, setWeatherInfo] = useState(null);
+    const [error, setError] = useState('');
+    const [isClicked, setIsClicked] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const tempConverter = () => {
+        if (weatherInfo) {
+            const kelvin = weatherInfo.data.main.temp;
+            const celsius = kelvin - 273.15;
+            return celsius.toFixed(2); // Converting to 2 decimal places
+        }
+        return null;
+    };
+
+    const handleCityChange = (event) => {
+        setCity(event.target.value);
+    };
+
+    const fetchWeather = async () => {
+        setLoading(true);
+        setError('');
+        try {
+            const response = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=763ca7fa30c677c83e7f122aa7aa5387`
+            );
+            setWeatherInfo(response);
+        } catch (error) {
+            setError(error.response.data.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleClick = () => {
+        fetchWeather();
+        setIsClicked(true);
+    };
+
+    return (
+        <div className='weather-container'>
+            <div className="input-container">
+                <input
+                    type="text"
+                    placeholder='Enter City Name'
+                    value={city}
+                    onChange={handleCityChange}
+                />
+                <button onClick={handleClick}>Get Weather</button>
+            </div>
+
+            <div className={isClicked ? "data-container-show" : "data-container"}>
+                {loading ? (
+                    <div className='loading-content'>
+                        <h4>Loading...</h4> 
+                        <span className='loading'></span>
+                    </div>
+                ) : weatherInfo ? (
+                    <div className="weather-info">
+                        <div className="city">
+                            <h3>City:</h3>
+                            <p>{weatherInfo.data.name}</p>
+                        </div>
+
+                        <div className="city">
+                            <h3>Temperature:</h3>
+                            <p>{tempConverter()} °C</p>
+                        </div>
+
+                        <div className="city">
+                            <h3>Status:</h3>
+                            <p>{weatherInfo.data.weather[0].description}</p>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="error-message">{error}</div>
+                )}
+            </div>
+        </div>
+    );
+}
